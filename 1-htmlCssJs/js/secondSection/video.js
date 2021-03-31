f9d6050290e760cd448b75c4113ba871022e86de
Gabriel Ruiz <51479498+GobsRuiz@users.noreply.github.com>
2021-03-31 09:12:45 -0300

    Video

--- 1-htmlCssJs/js/secondSection/video.js
+++ 1-htmlCssJs/js/secondSection/video.js
@@ -1,6 +1,7 @@
 function  video() {
 
     let video = document.querySelector("#secondSection-rightContent video")
+    let controls = document.getElementById("secondSection-rightContent-controls")
     let pauseStartBtn = document.getElementById("secondSection-rightContent-controls-pausePlayBtn")
     let progress = document.getElementById("secondSection-rightContent-controls-rightContent-progress-bar")
     let volume = document.getElementById("secondSection-rightContent-controls-rightContent-volume")
@@ -8,11 +9,10 @@ function  video() {
     
 
 
+    // Play and pause
     pauseStartBtn.addEventListener("click", (e) => {
         playPauseVideo()
     })
-    
-    // Play and pause
     function playPauseVideo() {
         if(video.paused){
             pauseStartBtn.innerHTML="pause";
@@ -25,16 +25,36 @@ function  video() {
     
     // Progress
     video.addEventListener("timeupdate", () => {
+        progressVideo()
+    })
+    function progressVideo() {
         let value = (video.currentTime / video.duration) * 100;
         progress.style.width = value + "%";
     
         if(video.ended) {
             pauseStartBtn.innerHTML="play_arrow";
         }
-    })
+    }
     
+
     // Volume
     volume.addEventListener("change", (e) => {
         video.volume = volume.value;
     })
+
+    // Fullscreen
+    fullScreen.addEventListener("click", () => {
+        fullscreenVideo()
+    })
+    function fullscreenVideo() {
+        if(!video.classList[0]){
+            video.classList.add("videoFullscreen");
+            controls.classList.add("videoFullscreen-controls");
+            fullScreen.innerHTML = "fullscreen_exit";
+        }else if(video.classList[0]){
+            video.classList.remove("videoFullscreen")
+            controls.classList.remove("videoFullscreen-controls")
+            fullScreen.innerHTML = "fullscreen";
+        }
+    }
 }video()
