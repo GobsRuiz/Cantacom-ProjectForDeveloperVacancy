6ac89291781f827474ee958b4b0af99d2c4400a9
Gabriel Ruiz <51479498+GobsRuiz@users.noreply.github.com>
2021-03-31 18:19:43 -0300

    Footer

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -5,17 +5,21 @@
     <v-main>
       <router-view></router-view>
     </v-main>
+
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import AppBar from './components/AppBar';
+import Footer from './components/Footer';
 
 export default {
   name: 'App',
 
   components: {
     AppBar,
+    Footer,
   },
 
   data: () => ({
---
+++ vuejs/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-footer color="#e3e3e3" class="py-6">
+      <div class="container text-center">
+        <v-btn
+            v-for="link in links"
+            :key="link"
+            class="text-caption text-capitalize"
+            color="#848484"
+            text
+        >
+            {{ link }}
+        </v-btn>
+      </div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+    data: () => ({
+      links: [
+        'Contact',
+        'Download',
+        'Press',
+        'Email',
+        'Support',
+        'Privacy Policy',
+      ],
+    }),
+}
+</script>
+
+<style>
+
+</style>
